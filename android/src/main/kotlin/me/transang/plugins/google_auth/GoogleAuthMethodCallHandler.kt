a2e276aed18431ed7c3e7cf8ac3cc4fa664abfa0
Tran Sang <me@transa.ng>
2023-04-10 01:24:01 +0900

    change buidl ver

--- android/src/main/kotlin/me/transang/plugins/google_auth/GoogleAuthMethodCallHandler.kt
+++ android/src/main/kotlin/me/transang/plugins/google_auth/GoogleAuthMethodCallHandler.kt
@@ -1,12 +1,11 @@
 package me.transang.plugins.google_auth
 
-import androidx.annotation.NonNull
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 
 class GoogleAuthMethodCallHandler(private val delegate: GoogleAuthDelegate) :
 	MethodChannel.MethodCallHandler {
-	override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: MethodChannel.Result) {
+	override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
 		when (call.method) {
 			METHOD_SIGN_IN -> //				List<String> requestedScopes = call.argument("scopes");
 //				String hostedDomain = call.argument("hostedDomain");
@@ -14,7 +13,7 @@ class GoogleAuthMethodCallHandler(private val delegate: GoogleAuthDelegate) :
 					val clientId: String? = call.argument ("clientId")
 					if (clientId == null) result.error("Error initializing sign in", "clientId is required", null)
 					else delegate.signIn(
-						clientId,
+						clientId!,
 						result
 					)
 				} catch (e: Exception) {
