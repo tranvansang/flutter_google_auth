70e11476d70481161eda53811707663c2f8fa758
Tran Sang <me@transa.ng>
2023-04-09 22:38:06 +0900

    add deps and dart

--- lib/google_auth.dart
+++ lib/google_auth.dart
@@ -1,8 +1,16 @@
-
-import 'google_auth_platform_interface.dart';
+import 'package:flutter/services.dart';
 
 class GoogleAuth {
-  Future<String?> getPlatformVersion() {
-    return GoogleAuthPlatform.instance.getPlatformVersion();
+  final methodChannel =
+      const MethodChannel('me.transang.plugins.google_auth/channel');
+
+  Future<String> signIn(String clientId) async {
+    return await methodChannel.invokeMethod('sign-in', {
+      'clientId': clientId,
+    });
+  }
+
+  Future<void> signOut() async {
+    await methodChannel.invokeMethod('sign-out');
   }
 }
